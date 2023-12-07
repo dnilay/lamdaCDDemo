25bf727f0dfd54cda8f785eaf6b3e78ca75180ff
Nilay Das <mailnilaydas@gmail.com>
2023-12-07 17:15:23 +0530

    initial xommit

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -6,5 +6,5 @@ ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     #  response = ec2.describe_availability_zones()
-    response = "Hello World v2"
+    response = "Hello World v1"
     return {"statusCode": 200, "body": json.dumps(response)}
