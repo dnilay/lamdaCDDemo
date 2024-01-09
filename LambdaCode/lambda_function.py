c427b7c25adce3a520851637de213f258f40112c
Nilay Das <mailnilaydas@gmail.com>
2024-01-09 22:29:27 +0530

    5th commit

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -5,6 +5,6 @@ ec2 = boto3.client('ec2')
 
 
 def lambda_handler(event, context):
-    #  response = ec2.describe_availability_zones()
-    response = "Hello World v4"
-    return {"statusCode": 200, "body": json.dumps(response)}
+     response = ec2.describe_availability_zones()
+    # response = "Hello World v4"
+     return {"statusCode": 200, "body": json.dumps(response)}
